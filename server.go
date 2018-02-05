26af8f934ef9ff3c40953845e817d7ed050d217e
Robert Catmull <catmulltrim@gmail.com>
2018-02-05 09:39:09 -0700

    fixing user issue

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"github.com/labstack/echo"
 	"meridianhomeforsale/api/controllers/garage"
 	"github.com/joho/godotenv"
+	"github.com/labstack/echo/middleware"
 )
 
 // todo.go
@@ -17,7 +18,13 @@ func main() {
 	// Create a new instance of Echo
 	server := echo.New()
 
+	server.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+	}))
+
 	garage.Routes(server)
+
 
 	server.Logger.Fatal(server.Start(":5000"))
 
